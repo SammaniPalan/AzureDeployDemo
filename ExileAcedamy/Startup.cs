25e0932e4f399524ebefdeaf3f3542de8ffe1877
Pavithra Sammani Palansuriya <spa@exilesoft.com>
2016-08-18 15:18:50 +0530

    First commit

---
+++ ExileAcedamy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExileAcedamy.Startup))]
+namespace ExileAcedamy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
